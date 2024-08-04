1fc3a8b06f45ee70bcfefefb00a46c97b8a7f6fa
José Miguel González Guzmán <107879363+JosemAVSG@users.noreply.github.com>
2024-08-04 00:06:28 -0500

    funciona en nativo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,28 +1,28 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native'
-import { Link, Slot } from 'expo-router'
+import { StatusBar,  Text, View } from 'react-native'
+import { Link } from 'expo-router'
 import React from 'react'
 
 const index = () => {
   return (
-    <>
-    <View className="flex-1 items-center justify-center bg-black">
+
+    <View className='flex items-center justify-center h-screen'>
         <Text>Aora</Text>
-        <StatusBar barStyle="dark-content" />
-        <Link href="./profile" style={{margin: 10, color: "#0a7ea4"}} >Profile</Link>
+        <StatusBar barStyle="default" />
+        <Link href="./profile" className="text-2xl text-blue" >Profile</Link>
     </View>
-    </>
+  
   )
 }
 
-export default index
+export default index;
 
-const styles = StyleSheet.create({
-    container:{
-        display: "flex",
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#F5FCFF",
-        padding: 10
-    }
-})
+// const styles = StyleSheet.create({
+//     container:{
+//         display: "flex",
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//         backgroundColor: "#F5FCFF",
+//         padding: 10
+//     }
+// })
