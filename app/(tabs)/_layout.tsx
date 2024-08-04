5ad14aa8c5cf406ca521aac297a6965ff20cc120
José Miguel González Guzmán <107879363+JosemAVSG@users.noreply.github.com>
2024-08-04 01:53:12 -0500

    adding assets

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
-import { Slot, Stack } from 'expo-router'
-import React from 'react'
+import { Slot, SplashScreen, Stack } from 'expo-router'
+import React, { useEffect } from 'react'
+import {useFonts} from 'expo-font'
 
 const _layout = () => {
+    const [fontsLoaded, error] = useFonts({
+      "Poppins": require("../../assets/fonts/Poppins-Medium.ttf"),
+      "Poppins-SemiBold": require("../../assets/fonts/Poppins-SemiBold.ttf"),
+      "Poppins-Bold": require("../../assets/fonts/Poppins-Bold.ttf"),
+      "Poppins-Black": require("../../assets/fonts/Poppins-Black.ttf"),
+      "Poppins-Medium": require("../../assets/fonts/Poppins-Medium.ttf"),
+      "Poppins-Regular": require("../../assets/fonts/Poppins-Regular.ttf"),
+      "Poppins-Light": require("../../assets/fonts/Poppins-Light.ttf"),
+      "Poppins-Thin": require("../../assets/fonts/Poppins-Thin.ttf"),
+      "Poppins-ExtraLight": require("../../assets/fonts/Poppins-ExtraLight.ttf"),
+      "Poppins-ExtraBold": require("../../assets/fonts/Poppins-ExtraBold.ttf"),
+    })
+
+    useEffect(() => {
+      if(error) throw error;
+
+      if(fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded,error])
+
+    if(!fontsLoaded) return null
   return (
     <Stack>
       <Stack.Screen
